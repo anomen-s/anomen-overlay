a6574aedf0b22196a65c469ad45735d6625d7eb2
ludek <ludek.h@gmail.com>
2010-06-12 12:07:41 +0200

    www-apps/pmwiki/cookbook/Geobox

--- www-apps/pmwiki/cookbook/Geobox/geobox.php
+++ www-apps/pmwiki/cookbook/Geobox/geobox.php
@@ -25,15 +25,18 @@ Markup('geobox','fulltext','/\(:geobox\s+(.*?)\s*:\)/e',
 
 function asint($m, $index) 
 {
+ //FIXME: convert ',' -> '.'
+  
   $res = 0;
-  if (isset($m[$index]))
-    $res = $m[$index];
+  if (isset($m[$index])) {
+    $res = strtr($m[$index], ',', '.');
+  }
   return $res;
 }
 
 function parse_coords($coords)
 {
-    $re_num = "\d+(?:.\d*)?";
+    $re_num = "\d+(?:[.,]\d*)?";
     $re_coord="
 	    ([-+]?${re_num})
 	    \s*
@@ -54,61 +57,107 @@ function parse_coords($coords)
 		    )
 		)
 	    )?";
-    $regex_pre = "(N|S|)\s*(${re_coord})\s*(E|W|)\s*(${re_coord})";
-    $regex_post = "()(${re_coord})\s*(N|S)\s*(${re_coord})\s*(E|W)";
-    $m[] = '';
-    if (preg_match("/^\s*${regex_pre}\s*\$/x", $coords, $m)) {
-	$res["result"] = 'PRE';
-	$res["pattern"] = $regexp_pre;
+    $regex_pre = "(N|S|)\s*(${re_coord})\s*;?\s*(E|W|)\s*(${re_coord})";
+    $regex_post = "()(${re_coord})\s*(N|S)\s*\;?s*(${re_coord})\s*(E|W)";
+    $m[] = array();
+    if (preg_match("/^\s*${regex_pre}\s*\$/xi", $coords, $m)) {
+	$res['result'] = 'PRE';
+	$res['pattern'] = $regexp_pre;
     }
-    else if (preg_match("/^\s*${regex_post}\s*\$/x", $coords, $m)) {
-	$res["result"] = 'POST';
-	$res["pattern"] = $regexp_post;
+    else if (preg_match("/^\s*${regex_post}\s*\$/xi", $coords, $m)) {
+	$res['result'] = 'POST';
+	$res['pattern'] = $regexp_post;
 	$m[1]=$m[6];$m[6]=$m[11]; // move directions
     } 
     else  {
-	$res["result"] = 0;
+	$res['result'] = "";
     }    
     
     $res[0] = asint($m, 3) + asint($m, 4)/60 + asint($m, 5)/(60*60);
     $res[1] = asint($m, 8) + asint($m, 9)/60 + asint($m, 10)/(60*60);
 
-    if ($m[1] == 'S') { $res[0] = -$res[0]; }
-    if ($m[6] == 'W') { $res[1] = -$res[1]; }
+    if (strtoupper($m[1]) == 'S') { $res[0] = -$res[0]; }
+    if (strtoupper($m[6]) == 'W') { $res[1] = -$res[1]; }
 
         
     return $res;
 }
 
-function convert($c)
+function floor0($foo) 
 {
-    $c['Nd'] = $c['N'] = $c[0];$c['S'] = -$c[0];
-    $c['Ed'] = $c['E'] = $c[1];$c['W'] = -$c[1];
+    if($foo > 0) { return floor($foo); }
+    else { return ceil($foo); }
+}
+
+function sign($foo) 
+{
+    if($foo < 0) { return "-"; } 
+    else { return ""; }
+}
+
+
+// FIXME:: N = (NSig*Ndi)°Nmi'Ns''  = (NSig*Ndi)°Nm
+function convert_coords($c)
+{
+    $c['LAT'] = ($c[0] > 0) ? "N" : "S";
+    $c['LON'] = ($c[0] > 0) ? "E" : "W";
+    
+    $c['NSig'] = sign($c[0]);
+    $c['ESig'] = sign($c[0]);
+    
+    $c['Nd'] = abs($c['N'] = $c[0]);$c['S'] = -$c[0];
+    $c['Ed'] = abs($c['E'] = $c[1]);$c['W'] = -$c[1];
+
+    //FIXME: negative numbers - do not return negative min, sec
 
     // convert to [Nd.]°[Nm.]'[Ns]" and  [Nd.]°[Nm]'
-    $c['Nd.'] = floor($c['Nd']);
-    $c['Nm'] = ($c['Nd']-$c['Nd.'])*60;
-    $c['Nm.'] = floor($c['Nm']);
-    $c['Ns'] = ($c['Nm']-$c['Nm.'])*60;
-    $c['Ns.'] = floor($c['Ns']);
+    $c['Ndi'] = floor0($c['Nd']);
+    $c['Nm'] = abs(($c['Nd']-$c['Ndi'])*60);
+    $c['Nmi'] = floor0($c['Nm']);
+    $c['Ns'] = ($c['Nm']-$c['Nmi'])*60;
+    $c['Nsi'] = floor0($c['Ns']);
 
-    $c['Ed.'] = floor($c['Ed']);
-    $c['Em'] = ($c['Ed']-$c['Ed.'])*60;
-    $c['Em.'] = floor($c['Em']);
-    $c['Es'] = ($c['Em']-$c['Em.'])*60;
-    $c['Es.'] = floor($c['Es']);
+    $c['Edi'] = floor0($c['Ed']);
+    $c['Em'] = abs(($c['Ed']-$c['Edi'])*60);
+    $c['Emi'] = floor0($c['Em']);
+    $c['Es'] = ($c['Em']-$c['Emi'])*60;
+    $c['Esi'] = floor0($c['Es']);
 
     return $c;
+}
+
+function build_link($link, $coords) 
+{
+    foreach($coords as $ak => $av) {
+	$k[] = "/!$ak(?![a-z])/i";
+	$v[] = $av;
+    }
+    return preg_replace($k, $v, $link);
 }
 
 function geomaps($param)
 {
     $c = parse_coords($param);
-    if ($c['result'] == COORDS_INVALID) {
+    
+    if (empty($c['result'])) {
 	return "[Invalid ($param)]";
     } 
     else {
-	return "$param: [[mapy:Loc:${c[0]} ${c[1]}|mapy]][[gmaps:${c[0]} ${c[1]}|google]]";
+	$c = convert_coords($c);
+
+	$LINK_MAPY="http://www.mapy.cz/?query=Loc:${c['N']}%20${c['E']}";
+	$LINK_GMAPS="http://maps.google.com/?q=${c['N']}%20${c['E']}";
+	$LINK_AMAPY="http://amapy.atlas.cz/?q=${c['Ndi']}°${c['Nmi']}'${c['Ns']}%22${c['LAT']};${c['Edi']}°${c['Emi']}'${c['Es']}%22${c['LON']}";
+	$LINK_GC="http://www.geocaching.com/map/default.aspx?lat=${c['N']}&lng=${c['E']}";
+	$LINK_GC_LIST="http://www.geocaching.com/seek/nearest.aspx?lat=${c['N']}&lng=${c['E']}&f=1";
+	
+	// FIXME - sign OR NS/EW
+	//$COORDS="${c['NSig']}${c['Ndi']}°${c['Nm']} ${c['ESig']}${c['Edi']}°${c['Em']}";
+	
+	$COORDS=build_link("!NSig!Ndi°!Nm !ESig!Edi°!Em", $c);//
+#	return Keep("<span><i>$COORDS</i><a href='$LINK_MAPY'>mapy</a> <a href='$LINK_GMAPS'>gmaps</a></span>");
+	return "''$COORDS''  – [[$LINK_MAPY | mapy]] [[$LINK_GMAPS | gmaps]] [[$LINK_AMAPY | amapy]] [[$LINK_GC | gc]] [[$LINK_GC_LIST | gc list]]";
+	
     }
 }
 
