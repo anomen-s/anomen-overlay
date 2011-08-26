7bf9d8d725b337614da0b834ff7ce7fa660e1884
anomen <ludek.h@gmail.com>
2011-08-26 11:53:35 +0200

    www-apps/pmwiki - sortpagelines

---
+++ www-apps/pmwiki/cookbook/SortPageLines/sortpagelines.php
@@ -0,0 +1,58 @@
+<?php if (!defined('PmWiki')) exit();
+
+/*
+    SortPage
+
+    Copyright 2011 Anomen (ludek_h@seznam.cz)
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+*/
+
+$RecipeInfo['SortPageLines']['Version'] = '2011-08-24';
+
+if ($action == 'edit') {
+
+$HTMLHeaderFmt['sortpage'] = '
+<script type="text/javascript">
+// <![CDATA[
+
+
+function sortPage(order) {
+
+ var sortPage_fnc = function (a, b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+ }
+
+ var textField = document.getElementById("text");
+ var value = textField.value;
+
+ var mySplitResult = value.split("\n");
+ mySplitResult.sort(sortPage_fnc);
+ if (order == 1) {
+  mySplitResult.reverse();
+ }   
+ var value  = "";
+ for(i = 0; i < mySplitResult.length; i++) {
+  if (mySplitResult[i] != "") {
+     value= value + mySplitResult[i] + "\n";
+  }
+ }
+ textField.value = value;
+}
+// ]]> 
+</script>
+';
+
+
+$GUIButtons['sortpageAsc'] = array(750, '', '', '',
+ '<input type=\"button\" name=\"sortPageButtonAsc\" value=\"$[Sort lines ASC]\" onClick=\"sortPage(0);\" />');
+
+$GUIButtons['sortpageDesc'] = array(751, '', '', '',
+ '<input type=\"button\" name=\"sortPageButtonDesc\" value=\"$[Sort lines DESC]\" onClick=\"sortPage(1);\" />');
+
+}
+
