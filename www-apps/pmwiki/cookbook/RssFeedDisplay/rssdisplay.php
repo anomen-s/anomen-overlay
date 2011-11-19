d6bf84551baccc36830d7cc2662e21d434031be2
anomen <ludek.h@gmail.com>
2011-11-19 22:01:31 +0100

    www-apps/pmwiki - cookbook rss feeds

--- www-apps/pmwiki/cookbook/RssFeedDisplay/rssdisplay.php
+++ www-apps/pmwiki/cookbook/RssFeedDisplay/rssdisplay.php
@@ -1,8 +1,5 @@
 <?php if (!defined('PmWiki')) exit();
 /*
-     http://www.brambring.nl
-
-     $Id: rssdisplay.php 295 2008-11-30 14:16:50Z ludek $
 
     Syntax
     (:RSS http://url.to.rss.feed/feed.rss <what> <parameters> :)
@@ -16,35 +13,80 @@
 
     displays the pmwiki feed in short layout and  all lines in the feed
     (:RSS http://www.pmichaud.com/wiki/Pm/AllRecentChanges?action=rss short -1 :)
+
+    Copyright 2006-2011 Anomen (ludek_h@seznam.cz)
+    Copyright 2005 http://www.brambring.nl
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published
+    by the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
 */
 
-$RecipeInfo['RSSDisplay']['Version'] = '2011-10-13';
+$RecipeInfo['RSSDisplay']['Version'] = '2011-11-19';
 
-Markup('rssdisplay', 'fulltext', '/\(:RSS\s*(.*?)\s*:\)/e',"RSS('\$1')");
-
-define('MAGPIE_CACHE_AGE', 2*60*60);
-//define('MAGPIE_CACHE_DIR', "$FarmD/cache");
-define('MAGPIE_FETCH_TIME_OUT', 15);
-define('MAGPIE_GZIP', true);
+Markup('rssdisplay', 'fulltext', '/\(:RSS\s*(.*?)\s*:\)/e',"MagpieRSS('\$1')");
 
 
-function RSS($regex) {
-global $action;
-global $FarmD;
-global $MAGPIE_ERROR;
+SDV($MagpieDir, "$FarmD/cookbook/magpie");
 
-$OriginalError_reportingLevel = error_reporting();
-error_reporting(0);
+SDV($MagpieEnableCache, false);
+SDV($MagpieCacheAge, 2*60*60);
+SDV($MagpieCacheDir, "$FarmD/cache");
+
+SDV($MagpieFetchTimeout, 15);
+SDV($MagpieGzip, true);
+SDV($MagpieProxy, '');
+
+SDV($MagpieDefaultItems, 30);
+SDV($MagpieDefaultFormat, 'long');
+
+SDV($MagpieOutputEncoding, $Charset);
+
+
+$MagpieRssPhpIncluded = 0;
+
+function MagpieRssPhpInclude()
+{
+	global $MagpieRssPhpIncluded;
+	if (!empty($MagpieRssPhpIncluded)) {
+	    return;
+	}
+	global $MagpieDir;
+	global $MagpieEnableCache, $MagpieCacheAge, $MagpieCacheDir;
+	global $MagpieFetchTimeout, $MagpieGzip, $MagpieOutputEncoding, $MagpieProxy;
+
+	$MagpieRssPhpIncluded = 1;
+	define('MAGPIE_CACHE_ON', $MagpieEnableCache);
+	define('MAGPIE_CACHE_AGE', $MagpieCacheAge);
+	define('MAGPIE_CACHE_DIR', $MagpieCacheDir);
+
+	define('MAGPIE_FETCH_TIME_OUT', $MagpieFetchTimeout);
+	define('MAGPIE_GZIP', $MagpieGzip);
+	define('MAGPIE_PROXY', $MagpieProxy);
+	define('MAGPIE_OUTPUT_ENCODING', $MagpieOutputEncoding);
+
+	include("$MagpieDir/rss_fetch.inc");
+}
+
+function MagpieRSS($regex) {
+ global $action;
+ global $FarmD;
+ global $MAGPIE_ERROR;
+ global $MagpieDefaultItems, $MagpieDefaultFormat;
+
+ $OriginalError_reportingLevel = error_reporting();
+ error_reporting(0);
 # If you make a call to your own site
 # and you have any kind of WritePage action ( Lock(2) ) for example
 # because you do some kind of logging in a wiki-page.
 # the whole thing will deadlock, so remove any locks.
 # ReadPage will request a lock again when needed
-Lock(-1);
-$line="";
-if ( $regex == '' ) {
- $line.="Empty parameter in RSS ";
-} else {
+ Lock(-1);
+ $line="";
+ if ( $regex == '' ) {
+  $line.="Empty parameter in RSS ";
+ } else {
   $tokens=preg_split("/\s+/", $regex);
 
   $url      =html_entity_decode($tokens[0]);
@@ -52,12 +94,12 @@ if ( $regex == '' ) {
   $num_items=$tokens[2];
   $header   =$tokens[3];
 
-  if ( $what == '' ) {
-    $what='long';  // modified by anomen
+  if (empty($what)) {
+    $what=$MagpieDefaultFormat;
   }
 
-  if ( $num_items == 0 ) {
-    $num_items=30;  // modified by anomen
+  if (empty($num_items)) {
+    $num_items=$MagpieDefaultItems;
   }
   if ( $action && (  $action != 'browse'  ) ) {
     # this is important
@@ -65,7 +107,8 @@ if ( $regex == '' ) {
     #recursion untill the server blows
     $line.= "RSS Feed : $url";
   } else {
-//    include_once("$FarmD/cookbook/magpie/rss_fetch.inc");
+    MagpieRssPhpInclude();
+
     $rss = fetch_rss($url);
     if ($rss) {
       $title = $rss->channel['title'];
