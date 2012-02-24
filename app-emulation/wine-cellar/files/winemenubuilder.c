7b47dbdad01049c848bce4d8bdf2335242711fd6
anomen <ludek.h@gmail.com>
2012-02-24 23:16:23 +0100

    app-emulation/wine-cellar-20110816

---
+++ app-emulation/wine-cellar/files/winemenubuilder.c
@@ -0,0 +1,21 @@
+/* Fake winemenubuilder.exe program */
+
+/* Use MinGW to build it */
+
+#include <stdio.h>
+
+
+void main(int argc, char** argv)
+{
+    int i;
+
+    printf("*********\n");
+
+    printf("%s", argv[0]);
+
+    for (i = 1; i < argc; i++)
+	printf(" \"%s\"", argv[i]);
+
+    printf("\n");
+
+}
